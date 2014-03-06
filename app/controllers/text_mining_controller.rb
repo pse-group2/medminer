2147d95032fa375a01ac9b509110b3b662c7f0c7
awaelchli <adrian.waelchli@students.unibe.ch>
2014-03-06 15:37:37 +0100

    first gui version

---
+++ app/controllers/text_mining_controller.rb
@@ -0,0 +1,12 @@
+class TextMiningController < ApplicationController
+  def search
+    
+  end
+
+  def results
+  end
+  
+  def get
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Medminer::Application.routes.draw do
+  get "text_mining/search"
+  get "text_mining/results"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root :to => "text_mining#search"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
