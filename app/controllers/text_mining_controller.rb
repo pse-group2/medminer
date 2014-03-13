6d7b867c09ce017b3be78e428e02cd866e60c7c2
Nils Schnabel <nils.schnabel@students.unibe.ch>
2014-03-13 15:29:36 +0100

    nil: fix

--- app/controllers/text_mining_controller.rb
+++ app/controllers/text_mining_controller.rb
@@ -1,7 +1,7 @@
 class TextMiningController < ApplicationController
   def search
-   @text = params[:search_text]
-   client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "toor", :database => "dewiki")
+   @text = params[:search_text].nil? ? "" :  params[:search_text]
+   client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "team2", :database => "dewiki")
    
    @text = @text + Article.find(1).name
   end
