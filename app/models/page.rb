2066f8fb6a59e64164cd6a436d34d677f9b5ed3a
awaelchli <adrian.waelchli@students.unibe.ch>
2014-03-18 22:46:45 +0100

    minor cosmetics

--- app/models/page.rb
+++ app/models/page.rb
@@ -8,4 +8,8 @@ class Page < ActiveRecord::Base
     id = Revision.where(:rev_page => page_id).order(:rev_timestamp).last.rev_text_id
     text = Text.where(:old_id => id).first
   end
+  
+  def title
+    page_title
+  end
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -3,12 +3,15 @@ class Text < ActiveRecord::Base
   
   belongs_to :page
   
+  def text
+    old_text
+  end
   # Returns all the terms that occur in the text as an array.
   def terms
     terms_in_article = Array.new
     
     Term.all.each do |term|
-      if old_text.include? term.text
+      if text.include? term.text
         terms_in_article.push term.text
       end
     end
