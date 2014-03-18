abfd73e53dbb67c5e1066ddf8f06e0b6dc7b7316
awaelchli <adrian.waelchli@students.unibe.ch>
2014-03-18 19:44:20 +0100

    method words for a list of words in a term

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,4 +1,14 @@
+require 'treat'
+include Treat::Core::DSL
+
 class Term < ActiveRecord::Base
   
   has_many :article_term_links
+  
+  #Returns the words in this term
+  def words
+    phr = phrase text
+    phr.apply(:tokenize)
+    phr.to_a
+  end
 end
