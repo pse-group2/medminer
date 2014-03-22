0a080eed8bf04168e957488b70fa3d3ceb020197
awaelchli <adrian.waelchli@students.unibe.ch>
2014-03-22 20:40:38 +0100

    minor changes

--- app/models/term_tokenizer.rb
+++ app/models/term_tokenizer.rb
@@ -1,9 +1,11 @@
 class TermTokenizer
+  
   def self.fill_word_tables
     Term.all.each do |term|
-      words = term.words
+      proc = TextProcessor.new(term.text)
+      words = proc.words
       words.each do |word|
-        f = Word.find_or_create_by(text: word)
+        f = Word.find_or_create_by(text: word, count: 0)
         id = f.id
         if TextProcessor.is_noun(word)
           Noun.find_or_create_by(term_id: term.id, word_id: id)
