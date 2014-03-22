3d92d68030859c2f2a5ba839b5c98ab1e445adea
awaelchli <adrian.waelchli@students.unibe.ch>
2014-03-22 20:38:22 +0100

    adjusted to new indexing method

--- app/models/article_term_link.rb
+++ app/models/article_term_link.rb
@@ -1,22 +1,23 @@
 class ArticleTermLink < ActiveRecord::Base
 
-  belongs_to :article
+  belongs_to :page
   belongs_to :term
+  
+  after_initialize :defaults
+  
+  def defaults
+    self.ranking ||= 0
+  end
+  
   
   # Returns the articles that are indexed with the term of the given text, sorted by the ranking
   def self.getRelevantArticles(text)
-    articles = Array.new
 
-    Term.where(:text => text).each do |term|
-      where(:term_id => term.id).order(:ranking).each do |row|
-        article_id = row.article_id
-        article = Article.find_by_id(article_id)
-        articles.push(article)
-      end
-
+    term = Term.where(:text => text).first
+    unless term.nil?
+      results = where(:term_id => term.id).order(ranking: :desc)
     end
-
-    articles
+    results
   end
 
 end
