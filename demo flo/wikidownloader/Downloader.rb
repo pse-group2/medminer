6c082b633f7fa4c0be19d0ab25c341331c73155c
Florian Matter <folranpls@gmail.com>
2014-03-26 14:07:03 +0100

    updated version for wiki-DL script.
    now includes text_id. also includes smaller json file with more
    entries. plus: proper time formatting! :O

--- demo flo/wikidownloader/Downloader.rb
+++ demo flo/wikidownloader/Downloader.rb
@@ -25,7 +25,7 @@ class Downloader
 
   def startDownload
     @input.each do |name|
-      #p "#{@name}: #{(@c.to_f/@length.to_f*100).round(3)}%"
+      #p "#{@name} downloading article #{name["page_id"]} with name #{name["page_title"]}"
       @pct = (@c.to_f/@length.to_f*100).round(3)
       check = @client.query("SELECT * FROM page WHERE page_id = #{name["page_id"]}").count
       if check == 0
@@ -35,10 +35,10 @@ class Downloader
         doc.css('p,h1').each do |e|
           text << e.content
         end
-        pquery = "INSERT INTO page (page_id, page_title) VALUES(#{name["page_id"]}, '#{name["page_title"].gsub("'", %q(\\\'))}')"
-        tquery = "INSERT INTO text (page_id, content) VALUES(#{name["page_id"]}, '#{text.gsub("'", %q(\\\'))}')"
+        pquery = "INSERT INTO page (page_id, page_title, text_id) VALUES(#{name["page_id"]}, '#{name["page_title"].gsub("'", %q(\\\'))}', #{name["text_id"]})"
+        tquery = "INSERT INTO text (page_id, content, text_id) VALUES(#{name["page_id"]}, '#{text.gsub("'", %q(\\\'))}', #{name["text_id"]})"
         @client.query(pquery)
-        @client.query(tquery)
+        @client.query(tquery)    
       end
       @c+=1
     end
--- demo flo/wikidownloader/runDownloads.rb
+++ demo flo/wikidownloader/runDownloads.rb
@@ -1,27 +1,27 @@
 require './Downloader'
 require 'json'
 #WICHTIG: schema für die DB:
-#TABLE page mit ROW page_id (int) und ROW page_title (text)
-#TABLE text mit ROW page_id (int) und ROW content (medium_blob)
+#TABLE page mit ROW page_id (int), ROW page_title (text) und ROW text_id(int)
+#TABLE text mit ROW page_id (int), ROW content (medium_blob) und ROW text_id(int)
 #parameter
 threadNumber = 45;
 username = "root"
-password = "team2"
-dbname = "newtestwiki"
+password = "toor"
+dbname = "testwiki"
 
 #mysql client und artikel-IDs
 client = Mysql2::Client.new(:host => "localhost", :username => username, :password => password, :database => dbname)
-file = open("medarticles.json")
+file = open("wiki-IDs.json")
 json = file.read
 parsed = JSON.parse(json)
-totalLength = parsed.length.to_i
+p totalLength = parsed.length.to_i
 
 #erstelle downloader
 downloaders = []
 threads = []
 i = 0
 while i < threadNumber
-  downloaders << Downloader.new(dbname, username, password, parsed[(totalLength/threadNumber)*i+i..(totalLength/threadNumber)*(i+1)+i], "downloader#{i+1}")
+  downloaders << Downloader.new(dbname, username, password, parsed[(totalLength/threadNumber)*i+i..(totalLength/threadNumber)*(i+1)+i], "Downloader#{i+1}")
   i+=1
 end
 
@@ -45,5 +45,8 @@ while pct < 100
   sleep 1
 end
 #zeitmessung
-finish = Time.now
-p "Time elapsed: #{(finish-start)/60} minutes"
+t = finish-start
+mm, ss = t.divmod(60)            #=> [4515, 21]
+hh, mm = mm.divmod(60)           #=> [75, 15]
+dd, hh = hh.divmod(24)           #=> [3, 3]
+puts "Time elapsed: %d hours, %d minutes and %d seconds" % [hh, mm, ss]
