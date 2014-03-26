ecd2f20fdc49fad86ea54dd3cc75d5fe211fbbb1
awaelchli <adrian.waelchli@students.unibe.ch>
2014-03-26 13:28:36 +0100

    deleted unused tables

--- app/models/article.rb
+++
@@ -1,4 +0,0 @@
-class Article < ActiveRecord::Base
-  
-  has_many :article_term_links
-end
--- app/models/revision.rb
+++
@@ -1,7 +0,0 @@
-class Revision < ActiveRecord::Base
-  self.table_name = "revision"
-  
-  belongs_to :page
-  belongs_to :text
-  
-end
